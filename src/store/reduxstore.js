a98e664347a197be66aed1a520dcf8f8a74f5ceb
narendraojha8 <58454568+narendraojha8@users.noreply.github.com>
2024-06-29 22:44:58 +0530

    configure frontend,tailwind,adding routes

---
+++ src/store/reduxstore.js
@@ -0,0 +1,10 @@
+import { configureStore,combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({
+
+})
+
+export const store = configureStore({
+    reducer:rootReducer,
+    devTools:true
+})
